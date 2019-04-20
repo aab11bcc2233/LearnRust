121f47527c690637e1b124be79ffc84335e574cf
t.hcc@foxmail.com <t.hcc@foxmail.com>
2019-04-20 09:48:50 +0800

    macros

--- pancakes/src/main.rs
+++ pancakes/src/main.rs
@@ -1,7 +1,12 @@
+extern crate hello_macro;
 
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
 struct Pancakes;
 
 
 fn main() {
-    // Pancakes::hello_macro();
+    Pancakes::hello_macro();
 }
