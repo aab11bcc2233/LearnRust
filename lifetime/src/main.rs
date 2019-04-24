94bb4b7e95b13f222d7e5c54cfd4844021611df7
t.hcc@foxmail.com <t.hcc@foxmail.com>
2019-04-24 17:48:31 +0800

    tests

---
+++ lifetime/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("the longest string is {}", result);
+}
+
+fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
