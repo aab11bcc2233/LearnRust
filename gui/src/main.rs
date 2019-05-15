66bb027fc48b2c390127f129665d16ee5d849670
t.hcc@foxmail.com <t.hcc@foxmail.com>
2019-05-15 17:40:14 +0800

    gui

---
+++ gui/src/main.rs
@@ -0,0 +1,66 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("draw Button");
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("draw SelectBox")
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(
+                Button {
+                    width: 50,
+                    height: 30,
+                    label: "btn".to_string(),
+                }
+            ),
+            Box::new(
+                SelectBox {
+                    width: 75,
+                    height: 10,
+                    options: vec![
+                        String::from("Yes"),
+                        String::from("Maybe"),
+                        String::from("No")
+                    ],
+                }
+            )
+        ],
+    };
+
+    screen.run();
+}
