f282d75b27e74e99a59e7ac736b4424b6d8fe9b9
t.hcc@foxmail.com <t.hcc@foxmail.com>
2019-05-31 15:20:49 +0800

    dup1

--- dup1/src/main.rs
+++ dup1/src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
+use std::collections::HashMap;
 
 fn main() {
+    let mut counts: HashMap<String, u32> = HashMap::new();
+
     loop {
         let mut text = String::new();
 
@@ -8,11 +11,19 @@ fn main() {
             .read_line(&mut text)
             .expect("Failed to read line");
 
-        let text = text.trim();
+        let text = text.trim().to_string();
         if text == "q" {
-            println!("bye-bye!");
+            println!("input done!");
             break;
         }
-        println!("read line = {}", text);
+
+        let count = counts.entry(text).or_insert(0);
+        *count += 1;
+    }
+
+    for (line, count) in counts {
+        if count > 1 {
+            println!("{l:width$}{n}", l=line, width=4, n=count);
+        }
     }
 }
